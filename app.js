652f0d23df04c6f2f72684f5230d5e10ea3e830a
faizan-lala <faizankhaan6267448042@gmail.com>
2022-03-29 16:38:47 +0530

    second-commit

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express=require('express');
 const bodyParser=require('body-parser');
+const cors=require('cors');
 const userModel=require('./model/user.model');
 const mongoose=require('mongoose');
 mongoose.connect('mongodb+srv://faizankhan:786786@cluster0.5nhsv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
 const { request, response } = require('express');
 const port = process.env.PORT || 3000;
 const app=express();
+app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
