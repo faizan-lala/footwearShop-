85cfa7bcc3513e3f9b0f074747b5d6f0332d467e
faizan-lala <faizankhaan6267448042@gmail.com>
2022-03-30 23:54:37 +0530

    fourth-commit

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express=require('express');
 const bodyParser=require('body-parser');
 const cors=require('cors');
+const path=require('path');
+const { body } = require('express-validator');
+const multer = require("multer");
+var storage = multer.diskStorage(
+    {
+        destination: 'public/images',
+        filename: function (req, file, cb) {
+            cb(null, Date.now() + "-" + file.originalname);
+        }
+    }
+);
+
+var upload = multer({ storage: storage });
+
+const categoryModel=require('./model/category.model');
 const userModel=require('./model/user.model');
 const mongoose=require('mongoose');
 mongoose.connect('mongodb+srv://faizankhan:786786@cluster0.5nhsv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
@@ -8,6 +23,7 @@ const { request, response } = require('express');
 const port = process.env.PORT || 3000;
 const app=express();
 app.use(cors());
+app.use(express.static(path.join(__dirname,'public')));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
@@ -29,7 +45,20 @@ app.use('/signin',(request,response)=>{
     }).catch(err => {
         return response.status(500).json({ message: 'Oops something went wrong' });
     })
-})
+});
+ 
+app.use('/add-category',upload.single('categoryImage'),body('categoryName').not().isEmpty(),(request,response)=>{
+    categoryModel.create({
+        categoryName:request.body.categoryName,
+        categoryImage:"http://localhost:3000/images/"+request.file.filename})
+    .then(result=>{
+      return response.status(201).json(result);
+    }).catch(err=>{
+        return response.status(403).json({message:'Opps Something went wrong'});
+    });
+});
+
+
 app.listen(port,()=>{
     console.log('server running');
 })
---
+++ model/category.model.js
@@ -0,0 +1,15 @@
+const { text } = require('body-parser');
+const mongoose=require('mongoose');
+
+const categorySchema=new mongoose.Schema({
+   categoryName:{
+       type:String,
+       required:true
+   },
+   categoryImage:{
+    type:String,
+       required:true
+   }
+});
+
+module.exports=mongoose.model('categories',categorySchema);
